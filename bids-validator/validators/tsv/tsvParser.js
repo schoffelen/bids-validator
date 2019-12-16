3513324f3d0be843989658e8f5b30764a3bf720b
Goldmund <matthewtzito@gmail.com>
2019-12-16 08:38:51 -0800

    prettify parseTSV

--- bids-validator/validators/tsv/tsvParser.js
+++ bids-validator/validators/tsv/tsvParser.js
@@ -8,14 +8,16 @@ function parseTSV(contents) {
     headers: [],
     rows: [],
   }
-  content.rows = contents.trim().split('\n')
-  .filter(row => row && !/^\s*$/.test(row))
-  .map(row => row.trim().split('\t'))
+  content.rows = contents
+    .trim()
+    .split('\n')
+    .filter(row => row && !/^\s*$/.test(row))
+    .map(row => row.trim().split('\t'))
   content.headers = content.rows.length ? content.rows[0] : []
 
   return {
     headers: content.headers,
-    rows: content.rows
+    rows: content.rows,
   }
 }
 module.exports = parseTSV
